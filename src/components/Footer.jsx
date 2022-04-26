ef3279f6a4287fc42cd750a568cd8e81d2d63c18
Bastian Rebolledo <74275595+Bestianbluns@users.noreply.github.com>
2022-04-26 01:51:19 -0400

    New Artist for Yelan Background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = ({ artist, linkArtist }) => {
   return (
-    <p className='w-full md:w-1/2 2xl:w-1/3 mx-auto mt-16 py-5 text-3xl bg-gradient-to-r from-fuchsia-400/20 to-fuchsia-400/50 text-white text-center rounded-xl'>🎨 Art by: <a className='hover:underline hover:decoration-white' href={linkArtist} target="_blank" rel="noreferrer">{artist}</a></p>
+    <p className='w-full md:w-1/2 2xl:w-1/3 mx-auto mt-16 py-5 text-3xl bg-gradient-to-r from-indigo-400/50 to-indigo-400/70 text-white text-center rounded-xl'>🎨 Art by: <a className='hover:underline hover:decoration-white' href={linkArtist} target="_blank" rel="noreferrer">{artist}</a></p>
   )
 }
 
