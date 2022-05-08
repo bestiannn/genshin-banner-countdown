cb4aee423bb7aa5903f894145128ad51844668c8
Bastian Rebolledo <74275595+Bestianbluns@users.noreply.github.com>
2022-05-08 01:48:26 -0400

    Infinite Banner xd

--- src/components/times/Minutes.jsx
+++ src/components/times/Minutes.jsx
@@ -4,7 +4,7 @@ import { containerClass, serverClass, valueClass } from "./ClassesForTime";
 const minutes = ({ minutes }) => {
   return (
     <div className={containerClass}>
-      //<p className={valueClass}>{ minutes >= 10 ? minutes : '0' + minutes }</p>
+      <!-- <p className={valueClass}>{ minutes >= 10 ? minutes : '0' + minutes }</p> -->
     <p className={valueClass}>xd</p>
       <span className={serverClass}>minutes</span>
     </div>
