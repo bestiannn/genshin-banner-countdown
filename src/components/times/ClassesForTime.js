1bfde7217f1cd3812d22dc8b7352f09e59c54485
bestianbluns <rebolledocofrebastian@gmail.com>
2022-07-02 23:35:46 -0400

    new data in public folder

--- src/components/times/ClassesForTime.js
+++
@@ -1,3 +0,0 @@
-export const containerClass = "bg-opacity-10 border-white border-2 rounded-xl p-2 sm:w-24 md:w-28 lg:w-32 xl:w-36 2xl:w-48";
-export const valueClass = "text-3xl md:text-4xl";
-export const serverClass = "text-xl md:text-2xl";
