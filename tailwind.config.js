bde720992194215d67138be11e51d078094f1985
bestianbluns <rebolledocofrebastian@gmail.com>
2022-07-24 18:39:28 -0400

    wtffffff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      "backgroundPosition":{
-        "mobile": "center left 20%",
-        "tablet": "center left 10%"
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
