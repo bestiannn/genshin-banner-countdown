1043af4713558211e280cba07dc7aa83bed1e563
bestianbluns <rebolledocofrebastian@gmail.com>
2022-08-06 03:53:17 -0400

    data config fixed

--- src/dataConfig.js
+++ src/dataConfig.js
@@ -2,14 +2,14 @@ import moment from "moment";
 
 const config = {
   // Header Data
-  title: "Genshin Impact 3.0 Banner Countdown",
-  subTitle: "Tighnari and Collei Banner Countdown",
+  title: "Genshin Impact 3.0 Countdown",
+  subTitle: "Sumeru, Tighnari and Collei Banner Countdown",
 
   // Time Data
   // Time is obtained from asia banner server
-  timeAsia: moment.utc("2022-08-23T22:00:00+08:00"),
-  timeEurope: moment.utc("2022-08-23T22:00:00+08:00").add(7, "hours"),
-  timeAmerica: moment.utc("2022-08-23T22:00:00+08:00").add(13, "hours"),
+  timeAsia: moment.utc("2022-08-24T11:00:00+08:00"),
+  timeEurope: moment.utc("2022-08-24T11:00:00+08:00"),
+  timeAmerica: moment.utc("2022-08-24T11:00:00+08:00"),
 
   // Artist Data
   artistData: {
