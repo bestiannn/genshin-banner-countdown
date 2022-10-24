f812d3c6f5f195667d3ba1eaa6393f32b68d291d
bestiannn <rebolledocofrebastian@gmail.com>
2022-10-24 02:12:07 -0300

    [3.2-1] Layla meta data removed

--- src/dataConfig.js
+++ src/dataConfig.js
@@ -7,9 +7,9 @@ const config = {
 
   // Time Data
   // sometimes the time finish first in the asian server
-  timeAsia: moment.utc("2022-11-01T17:00:00-03:00"),
-  timeEurope: moment.utc("2022-11-01T17:00:00-03:00"),
-  timeAmerica: moment.utc("2022-11-01T17:00:00-03:00"),
+  timeAsia: moment.utc("2022-11-02T11:00:00+08:00"),
+  timeEurope: moment.utc("2022-11-02T11:00:00+08:00"),
+  timeAmerica: moment.utc("2022-11-02T11:00:00+08:00"),
 
   // Artist Data
   artistData: {
