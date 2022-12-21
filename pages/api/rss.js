7308064e52bdd3ac1f186a00a9c0f5f891c4deae
bestiannn <rebolledocofrebastian@gmail.com>
2022-12-21 20:56:31 -0300

    [[3.3-2]] RSS fixed

--- pages/api/rss.js
+++ pages/api/rss.js
@@ -15,33 +15,15 @@ export default function handler(req, res) {
           <language>en-us</language>
           <item>
               <title>Asia</title>
-              <description>Time left for Asian banner</description>
-              <days>${Asia.days}</days>
-              <hours>${Asia.hours}</hours>
-              <minutes>${Asia.minutes}</minutes>
-              <seconds>${Asia.seconds}</seconds>
-              <totalSeconds>${Asia.totalSeconds}</totalSeconds>
-              <timeFormatted>${Asia.totalSeconds}</timeFormatted>
+              <description>${Asia.timeFormatted}</description>
           </item>
           <item>
               <title>Europe</title>
-              <description>Time left for European banner</description>
-              <days>${Europe.days}</days>
-              <hours>${Europe.hours}</hours>
-              <minutes>${Europe.minutes}</minutes>
-              <seconds>${Europe.seconds}</seconds>
-              <totalSeconds>${Europe.totalSeconds}</totalSeconds>
-              <timeFormatted>${Europe.totalSeconds}</timeFormatted>
+              <description>${Europe.timeFormatted}</description>
           </item>
           <item>
               <title>America</title>
-              <description>Time left for American banner</description>
-              <days>${America.days}</days>
-              <hours>${America.hours}</hours>
-              <minutes>${America.minutes}</minutes>
-              <seconds>${America.seconds}</seconds>
-              <totalSeconds>${America.totalSeconds}</totalSeconds>
-              <timeFormatted>${America.totalSeconds}</timeFormatted>
+              <description>${America.timeFormatted}</description>
           </item>
       </channel>
   </rss>`;
