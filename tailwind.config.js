431d3c08e110f1e4f6b9d62c585f3eda8b2950c0
bestiannn <rebolledocofrebastian@gmail.com>
2023-05-14 01:33:41 -0400

    [3.7-1] Banner changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        wallpaper: "url('/baizhu.png')",
+        wallpaper: "url('/kirara.png')",
       },
       backgroundPosition: {
         "center-wallpaper": "left 30% top 25%",
